be32b53b716ad5bd673271484931a64b61836fc6
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2022-10-05 14:18:49 +0200

    style: --fix

--- src/writeSnapshots.js
+++ src/writeSnapshots.js
@@ -4,18 +4,15 @@ const checkCI = require('./checkCI');
 const writeSnapshots = (snaps, snapshotFilePath) => {
   checkCI();
 
-  const snapsFileContent = Object.entries(snaps).reduce(
-    (prev, [name, value]) => {
-      const snapshot = JSON.stringify(value, null, '\t')
-        .replace(/\\n/g, '\n')
-        .replace(/`/g, '\\`')
-        .replace(/\${/g, '\\${')
-        .replace(/(^")|("$)/g, '`');
+  const snapsFileContent = Object.entries(snaps).reduce((prev, [name, value]) => {
+    const snapshot = JSON.stringify(value, null, '\t')
+      .replace(/\\n/g, '\n')
+      .replace(/`/g, '\\`')
+      .replace(/\${/g, '\\${')
+      .replace(/(^")|("$)/g, '`');
 
-      return `${prev}exports["${name}"] = ${snapshot};\n\n`;
-    },
-    ''
-  );
+    return `${prev}exports["${name}"] = ${snapshot};\n\n`;
+  }, '');
 
   fs.writeFileSync(snapshotFilePath, snapsFileContent);
 };
